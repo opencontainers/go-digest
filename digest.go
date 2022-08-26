ef310d11263ab79053cea9df19308ddf346fb3b7
Nima Talebi <github@nima.id.au>
2022-08-25 17:49:28 -0700

    feat: Add two abstraction helper methods
    These methods are `IsIdenticalTo` and `IsEmpty`.
    
    Signed-off-by: Nima Talebi <github@nima.id.au>

--- digest.go
+++ digest.go
@@ -30,7 +30,7 @@ import (
 //
 // The following is an example of the contents of Digest types:
 //
-// 	sha256:7173b809ca12ec5dee4506cd86be934c4596dd234ee82c0662eac04a8c2c71dc
+//	sha256:7173b809ca12ec5dee4506cd86be934c4596dd234ee82c0662eac04a8c2c71dc
 //
 // This allows to abstract the digest behind this type and work only in those
 // terms.
@@ -144,6 +144,16 @@ func (d Digest) Hex() string {
 
 func (d Digest) String() string {
 	return string(d)
+}
+
+// IsIdenticalTo compares this digest to any other, inclusive of the hashing algorithm used
+func (d Digest) IsIdenticalTo(o Digest) bool {
+	return string(d) == string(o)
+}
+
+// IsEmpty does what it says (an empty digest is an empty string)
+func (d Digest) IsEmpty() bool {
+	return len(d) == 0
 }
 
 func (d Digest) sepIndex() int {
